4c5767550a28a5e06588d632961de0890e362b23
Aesthir92 <119849743+Aesthir92@users.noreply.github.com>
2023-02-09 23:09:06 +0100

    Delete Javadoc directory

--- Javadoc/module-search-index.js
+++
@@ -1,1 +0,0 @@
-moduleSearchIndex = [{"l":"Carlos"}];updateSearchResults();
--- Javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"Carlos","l":"com.mycompany.cuentas"}];updateSearchResults();
